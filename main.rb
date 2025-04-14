5f83c174df34779ef2f0074aef2e5964766edad7
Thamska <165489803+HugoUddmar@users.noreply.github.com>
2025-04-14 14:01:36 +0200

    Menysystem och mål
    Olika nivåer med som ser olika ut och lagt till en räknare som säger hur många slag man gjort och ett mål i varje nivå.

--- main.rb
+++ main.rb
@@ -11,16 +11,21 @@ WindowHeight = 670
 set width: WindowWidth
 set height: WindowHeight
 
-$period = 3.1
+period = 3.1
+
 shot = false
 power = 1
 add = 0.5
-time = 0
-blocks = []
+
 collision = false
+
+$howmanyshots = 0
 $type_of_collision = nil
 $pos = nil
 $oldpos = nil
+$menu = true
+$level = 0
+$ballingoal = false
 #classes
 
 class Background
@@ -34,14 +39,75 @@ class Background
   end
 end
 
+class Menu
+  def initialize()
+  end
+
+  def draw()
+    #level1
+
+    Rectangle.new(
+      x:100, y:200, width:100,height:50,color:'white',z:0
+    )
+
+    Text.new(
+      'Level1',
+      x: 120, y: 212,
+      style: 'bold',
+      size: 20,
+      color: 'blue',
+      z: 10
+    )
+
+    #level2
+
+    Rectangle.new(
+      x:300, y:200, width:100,height:50,color:'white',z:0
+    )
+
+    Text.new(
+      'Level2',
+      x: 320, y: 212,
+      style: 'bold',
+      size: 20,
+      color: 'blue',
+      z: 10
+    )
+
+    #level3
+
+    Rectangle.new(
+      x:500, y:200, width:100,height:50,color:'white',z:0
+    )
+
+    Text.new(
+      'Level3',
+      x: 520, y: 212,
+      style: 'bold',
+      size: 20,
+      color: 'blue',
+      z: 10
+    )
+
+    #quit
+
+    Rectangle.new(
+      x:300, y:300, width:100,height:50,color:'white',z:0
+    )
+
+    Text.new(
+      'Quit game',
+      x: 300, y: 312,
+      style: 'bold',
+      size: 20,
+      color: 'blue',
+      z: 10
+    )
+  end
+end
+
 class Block
-  attr_accessor :block
-  attr_accessor :x
-  attr_accessor :y
-  attr_accessor :width
-  attr_accessor :height
   def initialize(x,y,width,height)
-    @collision = false
     @x = x
     @y = y
     @width = width
@@ -65,7 +131,7 @@ class Block
     blocky3 = @y + @height
   
     
-    if golfball.x1 >= blockx1 - golfball.width + 1 && golfball.x2 <= blockx2 + golfball.width - 1
+    if golfball.x1 >= blockx1 - golfball.width && golfball.x2 <= blockx2 + golfball.width
       if golfball.y1 >= blocky1 && golfball.y3 <= blocky3
         if golfball.x1 <= blockx2
           #Golfbollen åker vänster i ett block
@@ -111,7 +177,7 @@ class Block
         
             return true
           else
-            #Golfbollen åker vänster i ett block
+            #Golfbollen åker upp i ett block
         
             $type_of_collision = "up"
             $pos = $oldpos
@@ -124,10 +190,9 @@ class Block
         if golfball.y3 >= blocky1 && golfball.y1 <= blocky1
           nbr2 = golfball.y3 - blocky1
           if nbr2 > nbr1
-            #Golfbollen åker vänster i ett block
+            #Golfbollen åker höger i ett block
         
             $type_of_collision = "right"
-            p $type_of_collision
             $pos = $oldpos
          
             return true
@@ -135,7 +200,6 @@ class Block
             #Golfbollen faller ner i ett block
         
             $type_of_collision = "down"
-            p $type_of_collision
             $pos = $oldpos
          
             return true
@@ -143,18 +207,16 @@ class Block
         else
           nbr2 = blocky3 - golfball.y1
           if nbr2 > nbr1
-            #Golfbollen åker vänster i ett block
+            #Golfbollen åker höger i ett block
         
             $type_of_collision = "right"
-            p $type_of_collision
             $pos = $oldpos
         
             return true
           else
-            #Golfbollen åker vänster i ett block
+            #Golfbollen åker upp i ett block
         
             $type_of_collision = "up"
-            p $type_of_collision
             $pos = $oldpos
         
             return true
@@ -180,16 +242,32 @@ class Block
   end
 end
 
+class Howmanyshots
+  def draw()
+    Text.new(
+      "#{$howmanyshots}",
+      x: 0, y: 0,
+      style: 'bold',
+      size: 20,
+      color: 'blue',
+      z: 10
+    )
+  end
+end
+
 class Player
-  attr_accessor :x
-  attr_accessor :y
-  attr_accessor :width
-  attr_accessor :height
-  attr_accessor :golfball
-  def initialize()
-    @x = 100
-    @y = 100
-    @grav = 0
+  attr_reader :x
+  attr_reader :y
+  attr_reader :xspeed
+  attr_reader :yspeed
+  attr_reader :grav
+  attr_reader :width
+  attr_reader :height
+  attr_reader :golfball
+  def initialize(startx,starty)
+    @x = startx
+    @y = starty
+    @grav = 0.01
     @width = 20
     @height = 20
     @xmultiplier = 1
@@ -222,24 +300,24 @@ class Player
       @yspeed += strength * @ymultiplier/@zmulitplier
     end
 
-    if collision && @onetime
-      @onetime = false
-
-      if $type_of_collision == "left" || $type_of_collision == "right"
-        @xspeed *= -1
-      else
-        @yspeed *= -1
-        @grav *= -1
-      end
-    end
-
-    if collision 
+    if collision
       @y = $pos[1]
       @x = $pos[0]
 
-      @xspeed *= 0.7
-      @yspeed *= 0.7
-      @grav *= 0.7
+      if @onetime
+        @onetime = false
+
+        if $type_of_collision == "left" || $type_of_collision == "right"
+          @xspeed *= -1
+        else
+          @yspeed *= -1
+          @grav *= -1
+        end
+      end
+
+      @xspeed *= 0.5
+      @yspeed *= 0.5
+      @grav *= 0.5
 
       if @grav > -0.2 && @grav < 0
         @grav = 0
@@ -248,12 +326,9 @@ class Player
       @onetime = true
 
       if @grav < 7
-        @grav += 0.05
+        @grav += 0.051
       end
     end
-
-    @x += @xspeed
-    @y += @yspeed + @grav
 
     if @xspeed < 0.1 && @xspeed > -0.1
       @xspeed = 0.0
@@ -263,10 +338,60 @@ class Player
       @yspeed = 0.0
     end
 
-    @xspeed *= 0.99;
+    @xspeed *= 0.99
     @yspeed *= 0.99
+
+    @x += @xspeed
+    @y += @yspeed + @grav
   end
 end
+
+class Goal
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
+
+  def draw()
+    @block = Rectangle.new(
+    x:@x, y:@y, width:30,height:30,color:'yellow',z:5
+    )
+
+    Text.new(
+      "Goal",
+      x: @x, y: @y+8,
+      style: 'bold',
+      size: 12,
+      color: 'blue',
+      z: 10
+    )
+  end
+
+  def collisionDetection(golfball)
+    return golfball && collission_detected?(golfball) #Måste skriva && golfball för att kolla om den är initierad
+  end
+
+  def collission_detected?(golfball)
+    blockx1 = @x
+    blockx2 = @x + 30
+    blocky1 = @y
+    blocky3 = @y + 30
+  
+    
+    if golfball.x1 >= blockx1 - golfball.width && golfball.x2 <= blockx2 + golfball.width
+      if golfball.y3 >= blocky1 && golfball.y1 <= blocky3
+        #Golfbollen faller på ett block
+        
+        $type_of_collision = "down"
+        $pos = $oldpos
+        $ballingoal = true
+        return true
+      end
+    end
+    return false
+  end
+end
+
 
 class PowerMeter
   attr_accessor :x
@@ -287,9 +412,9 @@ class PowerMeter
     )
   end
 
-  def move(playerx,playery,playerwidth,playerheight)
-    @x = playerx + (playerwidth - @width)/2 + (Math.sin($period) * 30).to_i
-    @y = playery + (playerheight - @height)/2 + (Math.cos($period) * 30).to_i
+  def move(playerx,playery,playerwidth,playerheight,period)
+    @x = playerx + (playerwidth - @width)/2 + (Math.sin(period) * 30).to_i
+    @y = playery + (playerheight - @height)/2 + (Math.cos(period) * 30).to_i
   end
 end
 
@@ -298,71 +423,136 @@ end
 on :key_held do |event|
   case event.key
   when 'left'
-    $period += 0.1
+    period += 0.1
   when 'right'
-    $period -= 0.1
+    period -= 0.1
   when 'space'
-    if power == 20
-      add *= -1
-    elsif power == 0
-      add *= -1
+    if $player.xspeed == 0 && $player.yspeed == 0 && $player.grav == 0
+      if power == 20
+        add *= -1
+      elsif power == 0
+        add *= -1
+      end
+      power += add
     end
-    power += add
+  when 'escape'
+    $menu = true
+    $ballingoal = false
+    $howmanyshots = 0
+  end
+end
+
+on :mouse_down do |event|
+  case event.button
+  when :left
+    if event.x > 100 && event.x < 200 && event.y > 200 && event.y < 250 && $menu
+      $menu = false
+
+      $player = Player.new(100,100)
+      $blocks = []
+      $blocks << Block.new(0,WindowHeight-5,WindowWidth,20)
+      $blocks << Block.new(0,150,WindowWidth-50,20)
+      $blocks << Block.new(50,300,WindowWidth-50,20)
+      $blocks << Block.new(WindowWidth-5,0,20,WindowHeight)
+      $blocks << Block.new(300, 400, 20, 100)
+      $blocks << Block.new(0,0,20,WindowHeight)
+      $blocks << Block.new(0,0,WindowWidth,20)
+      $blocks << Goal.new(300,300)
+    elsif event.x > 300 && event.x < 400 && event.y > 200 && event.y < 250 && $menu
+      $menu = false
+
+      $player = Player.new(100,100)
+      $blocks = []
+      $blocks << Block.new(0,WindowHeight-5,WindowWidth,20)
+      $blocks << Block.new(0,150,WindowWidth-50,20)
+      $blocks << Block.new(50,300,WindowWidth-50,20)
+      $blocks << Block.new(WindowWidth-5,0,20,WindowHeight)
+      $blocks << Block.new(300, 400, 20, 100)
+      $blocks << Block.new(0,0,20,WindowHeight)
+      $blocks << Block.new(0,0,WindowWidth,20)
+      $blocks << Goal.new(300,100)
+    elsif event.x > 500 && event.x < 600 && event.y > 200 && event.y < 250 && $menu
+      $menu = false
+
+      $player = Player.new(100,100)
+      $blocks = []
+      $blocks << Block.new(0,WindowHeight-5,WindowWidth,20)
+      $blocks << Block.new(0,150,WindowWidth-50,20)
+      $blocks << Block.new(50,300,WindowWidth-50,20)
+      $blocks << Block.new(WindowWidth-5,0,20,WindowHeight)
+      $blocks << Block.new(300, 400, 20, 100)
+      $blocks << Block.new(0,0,20,WindowHeight)
+      $blocks << Block.new(0,0,WindowWidth,20)
+      $blocks << Goal.new(500,150)
+    elsif event.x > 300 && event.x < 400 && event.y > 300 && event.y < 350 && $menu
+      close
+    end
   end
 end
 
 on :key_up do |event|
   case event.key
   when 'space'
-    shot = true
+    if $player.xspeed == 0 && $player.yspeed == 0 && $player.grav == 0
+      shot = true
+      $howmanyshots += 1
+    end
   end
 end
 
 #update
-
-player = Player.new
+menu = Menu.new
+howmanyshots = Howmanyshots.new
 powerMeter = PowerMeter.new
 background = Background.new
 
-blocks << Block.new(0,WindowHeight-5,WindowWidth,20)
-blocks << Block.new(0,150,WindowWidth-50,20)
-blocks << Block.new(50,300,WindowWidth-50,20)
-blocks << Block.new(WindowWidth-5,0,20,WindowHeight)
-blocks << Block.new(300, 400, 20, 100)
-blocks << Block.new(0,0,20,WindowHeight)
-blocks << Block.new(0,0,WindowWidth,20)
-
 update do
   clear
-  background.draw()
-  #p "x:#{player.x }"
-  #p "y: #{player.y}"
 
-  i = 0
-  collision = false
-  while i < blocks.length
-    blocks[i].draw
-    if blocks[i].collisionDetection(player.golfball) && !collision
+  background.draw()
+
+  if $ballingoal
+    Text.new(
+      "Yay! you did it, it took #{$howmanyshots} shots, press esc to go back to menu",
+      x: 5, y: 400,
+      style: 'bold',
+      size: 20,
+      color: 'white',
+      z: 10
+    )
+  elsif $menu
+    menu.draw
+  else
+    i = 0
+    collision = false
+    while i < $blocks.length
+      $blocks[i].draw
+      if $blocks[i].collisionDetection($player.golfball) && !collision
+        collision = true
+      end
+      i += 1
+    end
+
+    if !collision
+      $oldpos = [$player.x,$player.y]
+    end
+
+    if $player.xspeed == 0 && $player.yspeed == 0 && $player.grav == 0
       collision = true
     end
-    i += 1
-  end
 
-  #p $type_of_collision
+    $player.move(shot,power,powerMeter.x,powerMeter.y,collision)
+    powerMeter.move($player.x,$player.y,$player.width,$player.height,period)
 
-  if !collision
-    $oldpos = [player.x,player.y]
-  end
+    $player.draw()
+    powerMeter.draw(power)
 
-  player.move(shot,power,powerMeter.x,powerMeter.y,collision)
-  powerMeter.move(player.x,player.y,player.width,player.height)
+    howmanyshots.draw()
 
-  player.draw()
-  powerMeter.draw(power)
-
-  if shot 
-    shot = false
-    power = 1
+    if shot 
+      shot = false
+      power = 1
+    end
   end
 end
 
